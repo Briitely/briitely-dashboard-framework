8ea15de0430da1ddfbc4710831b5e0232a8136e1
Lana Walsh <info@briitely.com>
2026-07-24 11:06:34 -0600

    Add Briitely revenue field configuration

---
+++ config/revenue.ts
@@ -0,0 +1,9 @@
+export const revenueFields = {
+  contractStart: "Dj4nv2hRJJHaVvFv5inf",
+  cancelled: "HUh7k0BSaf0NzAzHobpT",
+  referralSource: "ojJ816hlcxer2BH3Lj9T",
+  mrr: "tgyWj8d4uc7vWUFy1hgn",
+  package: "Wcw1wNKccnUaSIXimTx1",
+  oneTimeFee: "kQZPEXLVMCUGw8eQMRi7",
+  wonDate: "VfCcsa3PkHcUCZ9sGPie",
+} as const;
